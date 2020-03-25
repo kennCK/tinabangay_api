6b7fded08c6f3a6cb1248f046a72fbd38321c6e6
kennCK <kennettecanales@gmail.com>
2020-03-25 10:42:12 +0800

    updte

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Patient;
-class PatientController extends Controller
+class PatientController extends APIController
 {
   function __construct(){
     $this->model = new PatientController();
