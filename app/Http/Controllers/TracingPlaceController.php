a382843144127ffae802d6cdaec99923fee24458
kennCK <kennettecanales@gmail.com>
2020-03-25 10:16:08 +0800

    update

--- app/Http/Controllers/TracingPlaceController.php
+++ app/Http/Controllers/TracingPlaceController.php
@@ -8,16 +8,25 @@ use App\Patient;
 use Illuminate\Support\Facades\DB;
 class TracingPlaceController extends APIController
 {
-  public function places(){
+  public function places(Request $request){
+    $data = $request->all();
     $positiveUser = DB::table('visited_places AS T1')
-                      ->join('patients AS T2','T2.account_id','=','T1.account_id')
-                      ->where('T2.status','=','positive')
-                      ->whereColumn('T2.updated_at','<=','T1.created_at')
-                      ->whereNull('T2.deleted_at')
-                      ->whereNull('T1.deleted_at')
-                      ->select('T1.*')->get();
+      ->join('patients AS T2','T2.account_id','=','T1.account_id')
+      ->where('T2.status','=',$data['status'])
+      ->whereNull('T2.deleted_at')
+      ->whereNull('T1.deleted_at')
+      ->select('T1.*')->get();
     
-    $this->response['data'] = $positiveUser->groupBy('locality');
+    $positiveUser = $positiveUser->groupBy('route');
+    $array = array();
+    foreach ($positiveUser as $key => $value) {
+      $place = VisitedPlace::where('route', '=', $key)->first();
+      $place['size'] = VisitedPlace::where('route', '=', $key)->count();
+      $array[] = $place;
+    }
+    $keys = array_column($array, 'size');
+    array_multisort($keys, SORT_DESC, $array);
+    $this->response['data'] = $array;
     return $this->response();
   }
 }
