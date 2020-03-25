2eb0d47dd906da59e7f4cb645c9aabddb614b5e8
FLance03 <flancec03@gmail.com>
2020-03-25 14:51:35 +0800

    PatientController

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Patient;
+use App\VisitedPlace;
+
 class PatientController extends APIController
 {
   function __construct(){
-    $this->model = new PatientController();
+    $this->model = new Patient();
   }
 
   public function retrieve(Request $request){
     $data = $request->all();
-    $this->retrieveDB($data); // $this->response['data']
+    $this->retrieveDB($data); 
+
+    $i = 0;
+    $data = $this->response['data'];
+    foreach ($data as $key => $value) {
+      $place[$i]['places'] = VisitedPlace::where('account_id', '=', $key['account_id'])->get();
+      $i++;
+    }
 
     return $this->response();
   }
