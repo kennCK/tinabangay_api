2ddf4dc72d21226595ba6f65154563245c9ef075
kennCK <kennettecanales@gmail.com>
2020-03-25 17:05:32 +0800

    update

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
 
 
         Increment\Account\AccountServiceProvider::class,
+        Increment\Common\Invitation\InvitationServiceProvider::class,
 
         /*
          * Application Service Providers...
