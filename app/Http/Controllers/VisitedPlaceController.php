2e19a41cae2c24e9eeae308c8aa61fc0388b7f53
kennCK <kennettecanales@gmail.com>
2020-03-25 15:50:57 +0800

    update

--- app/Http/Controllers/VisitedPlaceController.php
+++ app/Http/Controllers/VisitedPlaceController.php
@@ -6,15 +6,21 @@ use Illuminate\Http\Request;
 use App\VisitedPlace;
 class VisitedPlaceController extends APIController
 {
+  public $tracingPlaceController = 'App\Http\Controllers\TracingPlaceController';
+  
   function __construct(){
     $this->model = new VisitedPlace();
   }
 
   public function retrieve(Request $request){
     $data = $request->all();
-    $this->retrieveDB($data); // store to $this->response['data'];
-    // loop
-    // check per route if existed on affected areas
+    $this->retrieveDB($data); // store to 
+    $data = $this->response['data'];
+    $i = 0;
+    foreach ($data as $key => $value) {
+      $this->response['data'][$i]['status'] = app($this->tracingPlaceController)->getStatus($data[$i]);
+      $i++;
+    }
     return $this->response();
   }
 
