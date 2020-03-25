3baea55e249dfe4913c9bcda94e1a33425da41fb
Kennette Canales <kennettecanales@gmail.com>
2020-03-25 14:31:28 +0800

    update

--- app/Http/Controllers/VisitedPlaceController.php
+++ app/Http/Controllers/VisitedPlaceController.php
@@ -10,4 +10,12 @@ class VisitedPlaceController extends APIController
     $this->model = new VisitedPlace();
   }
 
+  public function retrieve(Request $request){
+    $data = $request->all();
+    $this->retrieveDB($data); // store to $this->response['data'];
+    // loop
+    // check per route if existed on affected areas
+    return $this->response();
+  }
+
 }
