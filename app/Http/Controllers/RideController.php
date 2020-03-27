58272f2b7d3eea15264220da0175da8983ba9698
kennCK <kennettecanales@gmail.com>
2020-03-27 17:35:23 +0800

    update

--- app/Http/Controllers/RideController.php
+++ app/Http/Controllers/RideController.php
@@ -9,4 +9,18 @@ class RideController extends APIController
   function __construct(){
     $this->model = new Ride();
   }
+
+  public function retrieve(Request $request){
+    $data = $request->all();
+    $this->retrieveDB($data);
+    $i = 0;
+    $data = $this->response['data'];
+    foreach ($data as $key) {
+      $data[$i]['status'] = 'negative'; // work on this later
+      $data[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $key['created_at'])->copy()->tz($this->response['timezone'])->format('F j, Y h:i A');
+      $i++;
+    }
+    $this->response['data'] = $data;
+    return $this->response();
+  }
 }
