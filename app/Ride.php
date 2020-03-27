5215a00572ff5fbf46385b2deeca74c2e5969d07
kennCK <kennettecanales@gmail.com>
2020-03-27 12:46:19 +0800

    udpate

--- app/Ride.php
+++ app/Ride.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Ride extends APIModel
 {
     protected $table = 'rides';
-    protected $fillable = ['account_id', 'owner_id', 'transportation_id'];
+    protected $fillable = ['account_id', 'owner_id', 'transportation_id', 'payload', 'from', 'from_date_time', 'to', 'to_date_time', 'type', 'date'];
 }
