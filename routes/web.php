9045474747c266ce152bd13657d624976a6f942f
kennCK <kennettecanales@gmail.com>
2020-03-27 09:31:11 +0800

    udpate

--- routes/web.php
+++ routes/web.php
@@ -176,3 +176,12 @@ Route::post($route.'retrieve', $controller."retrieve");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller.'test');
+
+//Ride History Controller
+$route = env('PACKAGE_ROUTE', '').'/ride_history/';
+$controller = 'RideHistoryController@';
+Route::post($route.'create', $controller."create");
+Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'update', $controller."update");
+Route::post($route.'delete', $controller."delete");
+Route::get($route.'test', $controller.'test');
