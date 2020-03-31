f6082af415a32c5740e8767dd37968611525b862
JustinRaz <justinrazmont@gmail.com>
2020-03-31 15:41:53 +0800

    please verify getHistory function in Tracing Controller

--- app/Http/Controllers/TracingController.php
+++ app/Http/Controllers/TracingController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 use App\VisitedPlace;
 use App\Patient;
 use App\UserInformation;
+
+//for getHistory()
+use App\Account;
+use App\Temperature;
+use App\Ride;
+
 use DB;
 
 class TracingController extends APIController
@@ -34,5 +40,36 @@ class TracingController extends APIController
         //     dd($jsonData);
         // }
      
+    }
+
+    function getHistory($username, $agent_id){
+      // retrieve the history of visited places, temperature 
+      // and transportation 
+      // of the user by passing 
+      // the username and id of the agent
+
+      $agentExists = Account::where('id','=', $agent_id)->where('account_type','=', 'AGENCY')->get();
+      
+      $this->response['data'] = null;
+      if(sizeof($agentExists) > 0){
+        $userExists = Account::where('id', '=', $username)->where('account_type', '=','USER')->get();
+        if(sizeof($userExists) > 0){
+          $this->response['data']['visited_places'] = Visitedplace::where('account_id', '=', $userExists[0]['id'])->get();
+          $this->response['data']['temperature_with_location'] = DB::table('temperatures')
+            ->join('temperature_locations', 'temperatures.id','=', 'temperature_locations.temperature_id')
+            ->select('temperatures.id', 'temperatures.account_id', 'temperatures.added_by', 'temperatures.value', 'temperatures.remarks', 'temperature_locations.longitude', 'temperature_locations.latitude', 'temperature_locations.route', 'temperature_locations.locality', 'temperature_locations.country', 'temperature_locations.region')
+            ->where('account_id', '=', $userExists[0]['id'])
+            ->get();
+          $this->response['data']['rides'] = Ride::where('account_id', '=', $userExists[0]['id'])->get();
+        }else{
+          $this->response['error'] = 'User not found';
+        }
+      }else{
+        $this->response['error'] = 'Agent not found';
+      }
+
+      return $this->response();
+
+
     }
 }
