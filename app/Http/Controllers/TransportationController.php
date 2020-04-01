72355a6d0bffe88e7cdee112c11f417b485c5fbd
kennCK <kennettecanales@gmail.com>
2020-04-01 19:13:44 +0800

    udapte

--- app/Http/Controllers/TransportationController.php
+++ app/Http/Controllers/TransportationController.php
@@ -9,4 +9,9 @@ class TransportationController extends APIController
   function __construct(){
     $this->model = new Transportation();
   }
+
+  public function getByParams($column, $value){
+    $result = Transportation::where($column, '=', $value)->get();
+    return (sizeof($result) > 0) ? $result[0] : null;
+  }
 }
