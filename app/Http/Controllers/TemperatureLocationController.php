542345a33164a9ba618e1d2d0a91ab0e176bd24c
kennCK <kennettecanales@gmail.com>
2020-04-01 08:19:05 +0800

    update

--- app/Http/Controllers/TemperatureLocationController.php
+++ app/Http/Controllers/TemperatureLocationController.php
@@ -9,8 +9,8 @@ class TemperatureLocationController extends APIController
   function __construct(){
     $this->model = new TemperatureLocation();
   }
+  
   function retrieve(Request $request){
-
     $resp= TemperatureLocation::with("temperature")->get();
     $this->retrieveDB($resp);
     return $this->response(); 
