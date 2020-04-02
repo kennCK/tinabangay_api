f8c10be977a85e6fdd9a540642f4f99cc6b3945f
kennCK <kennettecanales@gmail.com>
2020-04-02 11:25:52 +0800

    add location

---
+++ app/Location.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends APIModel
+{
+  protected $table = 'locations';
+  protected $fillable = ['account_id', 'longitude', 'latitude', 'route', 'locality', 'country', 'region'];
+}
