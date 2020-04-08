ef2e95fa8de10697cf50a0a0e0532091fba64519
FLance03 <flancec03@gmail.com>
2020-04-08 19:57:04 +0800

    added column remarks in patients table

--- app/Patient.php
+++ app/Patient.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends APIModel
 {
   protected $table = 'patients';
-  protected $fillable = ['account_id', 'added_by', 'status'];
+  protected $fillable = ['account_id', 'added_by', 'status', 'remarks'];
   public function userInfo(){
     return $this-> hasOne('App\UserInformation','account_id','account_id');
   }
