fa31cb6e41a8ce71b7c7d7c78b8a46226c17a63d
kennCK <kennettecanales@gmail.com>
2020-04-12 21:44:50 +0800

    update

--- app/Http/Controllers/TransportationController.php
+++ app/Http/Controllers/TransportationController.php
@@ -25,21 +25,6 @@ class TransportationController extends APIController
     return $this->response();
   }
 
-  public function retrieveNotifications(Request $request){
-    $data = $request->all();
-    $this->retrieveDB($data);
-    $result = $this->response['data'];
-    if(sizeof($result) > 0){
-      $i = 0;
-      foreach ($result as $key) {
-        $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz($this->response['timezone'])->format('F j, Y h:i A');
-        $i++;
-      }
-    }
-    $this->response['data'] = $result;
-    return $this->response();
-  }
-
   public function getByParams($column, $value){
     $result = Transportation::where($column, '=', $value)->orderBy('updated_at', 'desc')->get();
     return (sizeof($result) > 0) ? $result[0] : null;
