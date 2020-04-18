035c4b4a5d12f6b346963f97b69da00d9f8a00e6
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-04-18 15:50:06 +0800

    add fillable

--- app/Patient.php
+++ app/Patient.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends APIModel
 {
   protected $table = 'patients';
-  protected $fillable = ['account_id', 'added_by', 'status','source', 'remarks'];
+  protected $fillable = ['account_id', 'code', 'added_by', 'status','source', 'remarks'];
   public function userInfo(){
     return $this-> hasOne('App\UserInformation','account_id','account_id');
   }
