7f58a979aebe0e8adecdfed52f17e18fcc41da79
keyrwinz <keyrwinfelisilda@gmail.com>
2020-04-29 18:42:57 +0800

    tracingController: getStatus for [patient, visitedplaces]

--- app/Http/Controllers/TracingController.php
+++ app/Http/Controllers/TracingController.php
@@ -12,9 +12,12 @@ use App\Temperature;
 use App\Ride;
 
 use DB;
+use Carbon\Carbon;
 
 class TracingController extends APIController
 {
+    public $tracingPlaceController = 'App\Http\Controllers\TracingPlaceController';
+
     public function tree(){
         $placesList = VisitedPlace::with('patients','userInfo')->get();
         $this->retrieveDB($placesList);
@@ -67,12 +70,69 @@ class TracingController extends APIController
     }
 
     public function getStatus(Request $request){
+      /**
+       * PRIORITY: patient > visited places > transportation > temperature
+       */
+      $radius = env('RADIUS');
+      if (!isset($radius)) {
+        throw new \Exception('No env variable for "RADIUS"');
+      }
+
+      if (isset($data['radius'])) {
+        $radius = $data['radius'];
+      }
+
+      $priorityStatus = array('death','positive','pui','pum','negative');
       $data = $request->all();
-      $status = 'negative';
+      $accountId = $data['id'];
+      $statuses = array(
+        'patient' => 'negative',
+        'visited_places' => 'negative',
+        'transportation' => 'negative',
+        'temperature' => 'negative'
+      );
+      
+      $now = Carbon::parse(Carbon::now()->format("Y-m-d H:i:s"));
+      $patientRecord = Patient::where('account_id', '=', $accountId)->first();
+      $visitedPlacesRecord = VisitedPlace::where('account_id', '=', $accountId)->get();
+
+      if ($patientRecord !== null) {
+        $patientRecordDate = Carbon::Parse(Carbon::createFromFormat('Y-m-d H:i:s', $patientRecord->created_at)->format("Y-m-d H:i:s"));
+        $daysAgo = $patientRecordDate->diffInDays($now);
+        if ($daysAgo < 14 && $patientRecord->status === 'positive') {
+          $this->response['data'] = array(
+            'from' => 'patient',
+            'status' => 'positive'
+          );
+          return $this->response();
+        } else if ($daysAgo < 14) {
+          $statuses['patient'] = $patientRecord->status;
+        }
+      } else if ($visitedPlacesRecord->count() > 0) {
+        foreach ($visitedPlacesRecord as $place) {
+          $visited_place_status = app($this->tracingPlaceController)->getStatus($place, $radius);
+          if (array_search($visited_place_status, $priorityStatus) < array_search($statuses['visited_places'], $priorityStatus)) {
+            $statuses['visited_places'] = $visited_place_status;
+          }
+        }
+      }
+
+
+      $status['key'] = null;
+      $status['status'] = 'negative';
+      foreach ($statuses as $key => $value) {
+        if (array_search($value, $priorityStatus) < array_search($status['status'], $priorityStatus)) { 
+          $status['key'] = $key;
+          $status['status'] = $value;
+        }
+      }
+
       $this->response['data'] = array(
-        'status' => $status
+        'from' => $status['key'],
+        'status' => $status['status']
       );
       return $this->response();
+      // return $status;
     }
 
     public function getStatusByAccountId($accountId){
