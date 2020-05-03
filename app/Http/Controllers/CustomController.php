097e6d4d82bd687620cf925c2d81e35d2a0dcf93
keyrwinz <keyrwinfelisilda@gmail.com>
2020-05-03 09:46:11 +0800

    remove retrieve from CustomController

--- app/Http/Controllers/CustomController.php
+++ app/Http/Controllers/CustomController.php
@@ -68,22 +68,6 @@ class CustomController extends APIController
       return $this->response();
     }
 
-    public function retrieve(Request $request){
-      $data = $request->all();
-      $this->model = new SubAccount();
-      $this->retrieveDB($data);
-      $result = $this->response['data'];
-
-      if(sizeof($result) > 0){
-        $i = 0;
-        foreach ($result as $key) {
-          $this->response['data'][$i]['account'] = $this->retrieveAccountDetails($result[$i]['account_id']);
-          $i++;
-        }
-      }
-      return $this->response();
-    }
-
     public function createDetails($accountId, $entry){
       $info = new AccountInformation();
       $info->account_id = $accountId;
