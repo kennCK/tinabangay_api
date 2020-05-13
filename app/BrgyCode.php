c47a301d53501de4eabce46a06810a06a78a0aee
keyrwinz <keyrwinfelisilda@gmail.com>
2020-05-13 14:03:03 +0800

    added url column in brgy_codes table

--- app/BrgyCode.php
+++ app/BrgyCode.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class BrgyCode extends APIModel
 {
   protected $table = 'brgy_codes';
-  protected $fillable = ['code', 'longitude', 'latitude', 'route', 'locality', 'country', 'region'];
+  protected $fillable = ['code', 'longitude', 'latitude', 'route', 'locality', 'country', 'region', 'url'];
 }
