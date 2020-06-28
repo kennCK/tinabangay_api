562e88cb09a582cc109091d488038a060d9ca83b
kennCK <kennettecanales@gmail.com>
2020-06-28 22:55:48 +0800

    update

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -13,6 +13,26 @@ class LocationController extends APIController
     );
   }
 
+  public function create(Request $request){
+    $data = $request->all();
+    if(isset($data['autogenerate'])){
+      $data['code'] = $this->generateCode();
+    }
+    $this->model = new Location();
+    $this->insertDB($data);
+    return $this->response();
+  }
+
+  public function generateCode(){
+    $code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"), 0, 32);
+    $codeExist = Location::where('code', '=', $code)->get();
+    if(sizeof($codeExist) > 0){
+      $this->generateCode();
+    }else{
+      return $code;
+    }
+  }
+
   public function getByParamsWithCode($column, $value){
     $result = Location::where($column, '=', $value)->where('code', '!=', null)->get();
     return sizeof($result) > 0 ? $result[0] : null;
