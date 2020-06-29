7d71c4a64033e36e05a0be7bd8c07527e94f0e92
kennCK <kennettecanales@gmail.com>
2020-06-29 22:14:40 +0800

    update

--- app/Location.php
+++ app/Location.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends APIModel
 {
   protected $table = 'locations';
-  protected $fillable = ['account_id', 'longitude', 'latitude', 'route', 'locality', 'country', 'region'];
+  protected $fillable = ['account_id', 'longitude', 'latitude', 'route', 'locality', 'country', 'region', 'code', 'assigned_code', 'payload'];
 }
