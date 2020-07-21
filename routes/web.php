f3a0a46ec0bf2025b4187d18f7113e78b04ec31f
kennCK <kennettecanales@gmail.com>
2020-07-21 18:40:58 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -287,3 +287,12 @@ Route::post($route.'retrieve', $controller."retrieve");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller.'test');
+
+//Post Controller
+$route = env('PACKAGE_ROUTE', '').'/posts/';
+$controller = 'PostController@';
+Route::post($route.'create', $controller."create");
+Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'update', $controller."update");
+Route::post($route.'delete', $controller."delete");
+Route::get($route.'test', $controller.'test');
